db0286c1a3dc31d5216500d3fe58df717d9aba72
Erick Coutinho <erickcoutinho08@hotmail.com>
2025-03-28 21:31:41 -0300

    upload projeto Chat Real Time

---
+++ client/src/components/join/Join.jsx
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react'
+import io from 'socket.io-client'
+
+export default function Join({ setChatVisibility, setSocket }) {
+  const usernameRef = useRef()
+
+  const handleSubmit = async () => {
+    const username = usernameRef.current.value
+    if (!username.trim()) return
+
+    const socket = await io.connect('http://localhost:3000')
+    socket.emit('set_username', username)
+    setSocket(socket)
+    
+    socket.on('connect', () => {
+      console.log('Conectado ao servidor com ID:', socket.id);
+    })
+
+    setChatVisibility(true)
+  }
+
+  return (
+    <div>
+      <h1>Join</h1>
+      <input type="text" ref={usernameRef} placeholder='Digite seu Nome' />
+      <button onClick={() => handleSubmit()}>Entrar</button>
+    </div>
+  )
+}
+''
